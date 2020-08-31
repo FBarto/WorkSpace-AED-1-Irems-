9d36600de76a01d30fc71ef4d2edd8b3dd66791c
s-passalacqua-iresm <69656420+s-passalacqua-iresm@users.noreply.github.com>
2020-08-31 20:16:52 -0300

    Update PrimeraTarea.py

--- PrimeraTarea.py
+++ PrimeraTarea.py
@@ -1,4 +1,4 @@
-def mayorDe10(num):
+def mayorDe10(num):  # Desde donde se llama a esta funcion?
 
     if (num >10):
         print(f"El Numero {num} Es mayor a 10")
@@ -7,39 +7,40 @@ def mayorDe10(num):
 
 """------------------------------------------------------------------------------------------------"""
 
-def divCuadrado(num1,num2):
+def divCuadrado(num1,num2): # Desde donde se llama a esta funcion?
     may=0
     men=0
     if(num1==num2):
         print(f"Los numero son iguales")
-        elif (num1>num2):
+        elif (num1>num2): # Esta linea esta mal identada, debería tener la misma cantidad de espacios adelante del elif que la funcion if de arriba
         may= num1
         men= num2
     else:
         may=num2
         men=num1
 
-    div = (may * may)/(men*men)
+    div = (may * may)/(men*men)  # Si los numeros son iguales va a mostrar los dos mensajes: 'Los numero son iguales' y tambien el mensaje 'Su resultado es: 1'
     print(f"Su resultado es: {div}")
 
 
 """----------------------------------------------------------------------------------------------"""
-def celcius(f):
+def celcius(f): # Desde donde se llama a esta funcion?
 
     c=(5/9)*(f-32)
     print(f"La temperatura expresada en grados celcius es {c}°")
 
 """----------------------------------------------------------------------------------------------"""
 def numero():
-    Num = int(input("Ingrese un numero"))
+    Num = int(input("Ingrese un numero")) 
         if(Num>0):
             print(f"El numero es positivo")
         elif(Num<0):
             print(f"El numero es negativo")
         else:
             print(f"Su numero es 0")
+    # Las lineas estan mal identadas; if, elif y else deben tener adelante la misma cantidad de espacios que Num
 """"--------------------------------------------------------------------------------------------"""
-def esIgual(num1, num2, num3):
+def esIgual(num1, num2, num3): # Desde donde se llama a esta funcion?
     if( (num1 + num2) == num3):
         print(f'La suma de {num1} y {num2} es igual a {num3}')
     else:
